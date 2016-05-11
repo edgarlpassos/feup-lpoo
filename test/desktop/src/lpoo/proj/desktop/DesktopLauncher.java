2b5f57478de09dfbd85293b93710d1dea577ad4a
Edgar Passos <epassos26@gmail.com>
2016-05-11 10:18:40 +0100

    Initial commit of gradle project

---
+++ test/desktop/src/lpoo/proj/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package lpoo.proj.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import lpoo.proj.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
