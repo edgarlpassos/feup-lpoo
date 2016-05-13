b2ff2316ea18cfb8eae33576783a0b639ff1e083
Edgar Passos <epassos26@gmail.com>
2016-05-13 18:26:24 +0100

    Setup basic game screen

--- test/desktop/src/lpoo/proj2/desktop/DesktopLauncher.java
+++ test/desktop/src/lpoo/proj2/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package lpoo.proj2.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import lpoo.proj2.Game;
+import lpoo.proj2.lpooGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Game(), config);
+		new LwjglApplication(new lpooGame(), config);
 	}
 }
