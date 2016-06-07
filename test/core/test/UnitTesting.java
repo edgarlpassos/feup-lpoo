ecb12dcdb3c67196dd28f970bb4f6272cad0aba0
Edgar Passos <epassos26@gmail.com>
2016-06-07 12:20:20 +0100

    Adding some missing files

---
+++ test/core/test/UnitTesting.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by epassos on 6/7/16.
+ */
+
+@RunWith(GdxTestRunner.class)
+public class UnitTesting {
+
+    @Test
+    public void testTest() {
+        assertTrue(true);
+    }
+}
